3422fb115a762a22fc89ff34717f972b1058b149
Alifyz <alifyzdev@gmail.com>
2020-05-17 00:13:44 -0300

    refactor: example home page layout

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:sensor_engine/sensor_engine.dart';
+import 'package:sensor_engine_example/pages/motion_page.dart';
 
 void main() {
   runApp(HomePage());
@@ -14,33 +15,52 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('SensorEngine'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Motion API'),
-                onPressed: () {},
-              ),
-              RaisedButton(
-                child: Text('Environment API'),
-                onPressed: () {},
-              ),
-              RaisedButton(
-                child: Text('Position API'),
-                onPressed: () {},
-              )
-            ],
+        body: Padding(
+          padding: EdgeInsets.only(left: 48, right: 48, bottom: 120),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  child: Icon(
+                    Icons.gps_fixed,
+                    color: Colors.deepOrangeAccent,
+                    size: 196,
+                  ),
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.deepOrangeAccent,
+                  child: Text('Motion API'),
+                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => MotionPage())),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.deepOrangeAccent,
+                  child: Text('Environment API'),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+                RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.deepOrangeAccent,
+                  child: Text('Position API'),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
 
 // class MyApp extends StatefulWidget {
 //   @override
